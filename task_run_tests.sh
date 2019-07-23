fc36f05903eac3e849618ccb98b0648399d09b1a
jackycsl <jcslmu@gmail.com>
2019-07-23 11:34:59 +0800

    Update task_run_tests.sh

--- task_run_tests.sh
+++ task_run_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/jackycsl/vs-go-demo/Godeps/_workspace
-cd gopath/src/github.com/jackycsl/vs-go-demo/
+# export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/jackycsl/vs-go-demo/Godeps/_workspace
+# cd gopath/src/github.com/jackycsl/vs-go-demo/
 
 go test ./...
